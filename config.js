6647116c95222a1c7c229cde4d4b7049a5b5aa51
jaedoo77-max <jaedoo77@gmail.com>
2026-08-22 09:36:32 +0900

    Preserve QR Laundry configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.QR_LAUNDRY_CONFIG = {
+  supabaseUrl: "https://qwwejwiiuzsitrycdnxh.supabase.co",
+  supabasePublishableKey: "sb_publishable_15rMns4_bbknjEM0s8GB4w_g7fNGMee",
+  shopName: "QR Laundry"
+};
